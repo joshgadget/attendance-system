06470322b2ece36e2543adb0a2794912033e8d42
joshgadget <joshuamomoh53@gmail.com>
2026-03-27 18:38:29 +0100

    Prepare frontend for GitHub Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import store from './redux/store';
 import { fetchCurrentUser } from './redux/slices/authSlice';
