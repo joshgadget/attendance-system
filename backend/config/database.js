f9dbf7799d8e075d8a0d67b98f1be3b18db3aa63
joshgadget <joshuamomoh53@gmail.com>
2026-05-20 13:07:04 +0100

    vvv

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,7 +15,7 @@ const sequelize = new Sequelize(
       ? {
           ssl: {
             require: true,
-            rejectUnauthorized: true,
+            rejectUnauthorized:false,
           },
         }
       : {},
